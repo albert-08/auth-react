e8d68ea2267610a6d3316a08b224f6d9fe53edc0
Alberto Flores <alberto.floreso94@gmail.com>
2023-06-23 12:30:00 -0600

    add routes and global state

---
+++ src/routes/ProtectedRoute.tsx
@@ -0,0 +1,8 @@
+import { Navigate, Outlet } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
+
+export default function ProtectedRoute() {
+  const auth = useAuth();
+
+  return auth.isAuthenticated ? <Outlet /> : <Navigate to="/" />
+}
